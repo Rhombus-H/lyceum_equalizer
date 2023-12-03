cfcaef2ed47aadc23ab0966c1833e30b90e22a89
Rhombus-H <rombworkbruh@gmail.com>
2023-12-03 19:03:05 +0300

    большое кол-во изменений интерфейса, переписываю логику в main

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import sys
 
 import test_qrc  # resource for a picture
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QColorDialog
-from widgets import main_window, equalize
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QColorDialog, QInputDialog
 
 import sqlite3
 import logging
 from pydub import AudioSegment
 from pydub import effects
+from transliterate import translit
 
 
 class Main(QMainWindow):
@@ -20,6 +20,8 @@ class Main(QMainWindow):
 
     def initUI(self):
         self.change_color_button.clicked.connect(self.change_color_of_buttons)
+        self.preset_delete_button.clicked.connect(self.delete_preset_button_clicked)
+        self.preset_add_button.clicked.connect(self.add_preset_button_clicked)
 
     def change_color_of_buttons(self):
         color = QColorDialog.getColor()
@@ -31,7 +33,34 @@ class Main(QMainWindow):
             self.accept_button.setStyleSheet(
                 "background-color: {}".format(color.name()))
 
+    def add_preset_button_clicked(self):
+        index = self.presets_list.currentRow()
+        preset_name, ok = QInputDialog.getText(self, "Новый пресет", "Название пресета")
+        try:
+            self.create_preset(preset_name)
+        except sqlite3.OperationalError:
+            # self.preset_name_field.setplaceholderText("Такой пресет уже существует.")
+            pass
+        if preset_name and ok:
+            self.presets_list.insertItem(index, preset_name)
 
+    def delete_preset_button_clicked(self):
+        index = self.presets_list.currentRow()
+        preset_name = self.presets_list.item(index)
+        if not self.presets_list.count():
+            alert = QMessageBox.about(self, "Ошибка", "")
+        else:
+            self.delete_preset(str(preset_name))
+            if preset_name is None:
+                return
+
+            question = QMessageBox.question(self, "Удаление пресета",
+            "Вы хотите удалить пресет " + preset_name.text() + '?',
+                                        QMessageBox.Yes | QMessageBox.No)
+
+            if question == QMessageBox.Yes:
+                item = self.presets_list.takeItem(index)
+                del item
     def get_slider_values_from_ui(self):
         """
         Retrieves the values of the sliders from the UI and returns them as a dictionary.
@@ -56,8 +85,8 @@ class Main(QMainWindow):
         Returns:
             list: A list of slider values retrieved from the database.
         """
-        cursor = self.connection.cursor('presets_base.db')
-        cursor.execute('SELECT freqency_1, freqency_2, freqency_3, freqency_4, freqency_5, freqency_6 FROM ?', (preset,))
+        cursor = sqlite3.connect('presets_base.db').cursor()
+        cursor.execute('SELECT frequency_1, frequency_2, frequency_3, frequency_4, frequency_5, frequency_6 FROM ?', (preset,))
         slider_values = cursor.fetchall()
         return slider_values
 
@@ -79,7 +108,7 @@ class Main(QMainWindow):
         Returns:
             list: A list containing the preamp value(s) associated with the preset.
         """
-        cursor = self.connection.cursor('presets_base.db')
+        cursor = sqlite3.connect('presets_base.db').cursor()
         cursor.execute('SELECT preamp FROM ?', (preset,))
         preamp_value = cursor.fetchall()
         return preamp_value
@@ -113,9 +142,36 @@ class Main(QMainWindow):
         value = self.get_preamp_value_from_db(preset)
         self.preamp_slider.setValue(value)
 
-    def create_preset(self):
-        pass
+    def format_preset_name(self, ru_preset_name):
+        """
+        Format the given preset name into database-allowed format.
 
+        Args:
+            ru_preset_name (str): The Russian preset name to be formatted.
+
+        Returns:
+            str: The formatted preset name in uppercase with underscores.
+        """
+        return '_'.join(translit(ru_preset_name, 'ru', reversed=True).split()).upper()
+
+    def create_preset(self, preset_name):
+        data = self.get_slider_values_from_ui() | {'preamp': self.get_preamp_value_from_ui}
+        cursor = sqlite3.connect('presets_base.db').cursor()
+        cursor.execute('SELECT name FROM presets_base WHERE name=?', (self, preset_name))
+        if cursor.fetchall():
+            return sqlite3.OperationalError
+        else:
+            cursor.execute(f'INSERT INTO presets VALUES ({format(tuple(data.values()))})')
+            cursor.fetchall()
+
+    def delete_preset(self, preset_name):
+        cursor = sqlite3.connect('presets_base.db').cursor()
+        cursor.execute('DELETE FROM presets WHERE name=?', (preset_name,))
+        cursor.fetchall()
+
+    def apply_preset(self):
+        self.set_preamp_value
+        self.set_slider_values
 
 
 if __name__ == "__main__":
